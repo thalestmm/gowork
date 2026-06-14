1939273192cec258acb0a3229d26303cbca9cd19
Thales Meier <meier.thales@gmail.com>
2026-06-13 22:44:08 -0300

    chore: update

--- workers.go
+++ workers.go
@@ -1,1 +1,16 @@
 package main
+
+type Worker interface {
+
+}
+
+type SimpleWorker struct {}
+
+type PriorityWorker struct {
+	MinPriority int
+	MaxPriority *int
+}
+
+type SpecificTaskWorker struct {
+	TaskSlug string
+}
